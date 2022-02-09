1b8d6534a042463b50939af7ae62bbf9abba8bd9
Igoryochek <83440261+Igoryochek@users.noreply.github.com>
2022-02-09 10:18:28 +1000

    Init

---
+++ PaymentSystems/PaymentSystems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PaymentSystems
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Order order = new Order(1522,12000);
+
+            PaymentSystem1 paymentSystem1 = new(order);
+            PaymentSystem2 paymentSystem2 = new(order);
+            PaymentSystem3 paymentSystem3 = new(order);
+
+            paymentSystem1.FormOrder();
+            paymentSystem2.FormOrder();
+            paymentSystem3.FormOrder();
+        }
+    }
+}
