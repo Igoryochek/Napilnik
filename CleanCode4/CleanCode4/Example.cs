eea61731605d124f1a8975be12d24bc300ddf36d
Igoryochek <83440261+Igoryochek@users.noreply.github.com>
2022-02-09 11:28:22 +1000

    Init

---
+++ CleanCode4/CleanCode4/Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode4
+{
+    class Example
+    {
+        private int _armySize = 10;
+        private int _coinNumber = 10;
+        private const string Name = "Vladislav";
+    }
+}
---
+++ CleanCode4/CleanCode4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CleanCode4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
