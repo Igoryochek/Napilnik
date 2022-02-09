ae238fe64e6f1b399befdc1856f6a3189703affa
Igoryochek <83440261+Igoryochek@users.noreply.github.com>
2022-02-09 10:42:17 +1000

    Init

---
+++ CleanCode1/CleanCode1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CleanCode1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static int GetValidNumber(int a, int b, int c)
+            {
+                if (a < b)
+                    return b;
+                else if (a > c)
+                    return c;
+                else
+                    return a;
+            }
+        }
+    }
+}
