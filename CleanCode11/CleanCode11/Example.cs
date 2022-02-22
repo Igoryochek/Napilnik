42a89a25200b24bfde3d049a6755354fa5713775
Igoryochek <83440261+Igoryochek@users.noreply.github.com>
2022-02-22 22:14:06 +1000

    Init

---
+++ CleanCode11/CleanCode11/Example.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanCode11
+{
+    class Example
+    {
+        public void OnEnable()
+        {
+            _effects.StartEnableAnimation();
+        }
+
+        public void OnDisable()
+        {
+            _pool.Free(this);
+        }
+    }
+}
